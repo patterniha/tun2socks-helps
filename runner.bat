1344e635e6f2e90f6e5cbddfcc25bf45f8055f3a
patterniha <71074308+patterniha@users.noreply.github.com>
2025-12-09 12:06:46 +0330

    Update runner.bat

--- runner.bat
+++ runner.bat
@@ -6,8 +6,10 @@
 
 "tun2socks.exe" -device wintun -proxy socks5://127.0.0.1:10808 -tcp-auto-tuning -tcp-rcvbuf 4m -tcp-sndbuf 4m -mtu 1500 -loglevel info -udp-timeout 30s
 
+pause
 
-:: it use default interface to send data, you can explicitly define (other) interface with #-interface "Wi-Fi"#
+
+:: it use default interface to send data, you can explicitly define (other) interface with for example #-interface "Wi-Fi"#
 :: but if interface is explicitly defined, udp-bind-IP-in-UDP-associate-response should be in explicted-defined-interface-IP-ranges (tun2socks bug)
 
 :: you can use socks-forwarder-addr/target-socks-in-addr for socks5 address, but care about udp-bind-IP-in-UDP-associate-response for differnt scenarios
