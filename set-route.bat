a1ecfeb481f11d5bb286fcff26987a241e90ab68
patterniha <71074308+patterniha@users.noreply.github.com>
2025-12-09 21:31:05 +0330

    Update set-route.bat

--- set-route.bat
+++ set-route.bat
@@ -8,7 +8,7 @@ if %errorlevel% neq 0 (
 )
 
 netsh interface ipv4 set address "wintun" static 10.10.14.1 255.255.255.0
-netsh interface ipv6 set address "wintun" fc00::10:10:14:1/64
+netsh interface ipv6 set address "wintun" fda0::1/64
 
 netsh interface ipv4 set dnsservers name="wintun" static 127.0.0.1 primary validate=no
 netsh interface ipv4 set dnsservers name="Wi-Fi" static 127.0.0.1 primary validate=no
@@ -17,7 +17,7 @@ netsh interface ipv6 set dnsservers name="Wi-Fi" static ::1 primary validate=no
 
 netsh interface ipv4 add route 0.0.0.0/1 "wintun" 10.10.14.1 metric=1
 netsh interface ipv4 add route 128.0.0.0/1 "wintun" 10.10.14.1 metric=1
-netsh interface ipv6 add route ::/1 "wintun" fc00::10:10:14:1 metric=1
-netsh interface ipv6 add route 8000::/1 "wintun" fc00::10:10:14:1 metric=1
+netsh interface ipv6 add route ::/1 "wintun" fda0::1 metric=1
+netsh interface ipv6 add route 8000::/1 "wintun" fda0::1 metric=1
 
 pause
