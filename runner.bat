cff1a7dce7251bc609ea94a268fecad385b7b39e
patterniha <71074308+patterniha@users.noreply.github.com>
2025-12-09 07:27:13 +0330

    Create WARNING.txt

--- runner.bat
+++ runner.bat
@@ -1,6 +1,8 @@
 :: https://github.com/xjasonlyu/tun2socks
 :: https://www.wintun.net/
 
+:: WARNING: socks5 UDP-IP should set to LAN-IP in target-config socks5 settings.
+
 @echo off
 
 "tun2socks.exe" -device wintun -proxy socks5://127.0.0.1:10808 -tcp-auto-tuning -tcp-rcvbuf 4m -tcp-sndbuf 4m -mtu 1500 -loglevel info -udp-timeout 30s -interface "Wi-Fi"
